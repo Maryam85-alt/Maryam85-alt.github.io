1e135f192dc5279ca5d51cff5ad3a38a7986a868
Maryam85-alt <mz9355519@gmail.com>
2025-08-17 19:36:36 +0500

    Update script.js

--- script.js
+++ script.js
@@ -1,24 +1,32 @@
 fetch('books.json')
-.then(response => response.json())
-.then(books => {
-  const bookGrid = document.querySelector('.book-grid');
-  books.forEach(book => {
-    const bookItem = document.createElement('div');
-    bookItem.classlist.add('book-item');
-    bookItem.innerHTML
-const searchInput = document.getElementById('search-input');
-searchInput.addEventListener('input', filterBook);
+    .then(response => response.json())
+    .then(books => {
+        const bookGrid = document.querySelector('.book-grid');
+        books.forEach(book => {
+            const bookItem = document.createElement('div');
+            bookItem.classList.add('book-item');
+            bookItem.innerHTML = `
+                <h3>${book.title}</h3>
+                <p>By ${book.author}</p>
+            `;
+            bookGrid.appendChild(bookItem);
+        });
 
-function filterBooks(){
-  const searchTerm = searchInput.value.trim().toLowerCase();
-  const bookItems = documents.querySelectorAll('.book-item');
-  bookItems.forEach(bookItem =>{
-    const bookTitle = bookItem.queryselector('h3').textContent.toLowerCase();
-    const bookAuthor = bookItem.queryselector('p').textContent.toLowerCase().replace('by ','')
-    if(bookTitle.includes(searchTerm)||bookAuthor.includes(searchTerm)){
-      bookItem.style.display = 'block';
-    }else{
-      bookItem.style.display='none';
-    });
-}
-      
+        const searchInput = document.getElementById('search-input');
+        searchInput.addEventListener('input', filterBooks);
+ function filterBooks() {
+            const searchTerm = searchInput.value.trim().toLowerCase();
+            const bookItems = document.querySelectorAll('.book-item');
+            bookItems.forEach(bookItem => {
+                const bookTitle = bookItem.querySelector('h3').textContent.toLowerCase();
+                const bookAuthor = bookItem.querySelector('p').textContent.toLowerCase().replace('by ', '');
+                if (bookTitle.includes(searchTerm) || bookAuthor.includes(searchTerm)) {
+                    bookItem.style.display = 'block';
+ } else {
+                    bookItem.style.display = 'none';
+                }
+            });
+        }
+    })
+    .catch(error => console.error('Error:', error));
+
