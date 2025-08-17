66d46d33e0403db7b335a725bb9221499904695b
Maryam85-alt <mz9355519@gmail.com>
2025-08-17 19:21:12 +0500

    Update script.js

--- script.js
+++ script.js
@@ -6,3 +6,12 @@ fetch('books.json')
     const bookItem = document.createElement('div');
     bookItem.classlist.add('book-item');
     bookItem.innerHTML
+const searchInput = document.getElementById('search-input');
+searchInput.addEventListener('input', filterBook);
+
+function filterBooks(){
+  const searchTerm = searchInput.value.trim().toLowerCase();
+  const bookItems = documents.querySelectorAll('.book-item');
+  bookItems.forEach(bookItem =>{
+    const bookTitle = bookItem.query selector('h3').textContent.toLowerCase();
+    const bookAuthor = bookItem
